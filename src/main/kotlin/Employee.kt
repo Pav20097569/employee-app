49e24fc2c80525b8c70fa3605fbb34e8a60c4b74
Pav20097569 <20097569@mail.wit.ie>
2023-02-11 17:08:32 +0000

    Updated Payslip UX

--- src/main/kotlin/Employee.kt
+++ src/main/kotlin/Employee.kt
@@ -20,24 +20,25 @@ class Employee(var firstName: String, var surname: String, var gender: Char, var
     fun getTotalMonthlyDeductions() = roundTwoDecimals((getMonthlyPRSI() + getMonthlyPAYE() + cycleToWorkMonthlyDeduction))
     fun getNetMonthlyPay() = roundTwoDecimals(roundTwoDecimals(getGrossMonthlyPay() - getTotalMonthlyDeductions()))
 
-    fun getPayslip() =
+    fun getPayslip() = //simplified the payslip and divided into easy to see sections
         """
-        ______________________________________________________________________
-         Monthly Payslip:             ${getFullName()}, ID: ${employeeID}                 
-        ______________________________________________________________________    
-              PAYMENT DETAILS (gross pay: ${getGrossMonthlyPay()})                                                                    
-        ______________________________________________________________________
-                   Salary: ${getMonthlySalary()}
-                   Bonus:  ${roundTwoDecimals(annualBonus / 12)}            
-        ______________________________________________________________________
-              DEDUCTION DETAILS (total Deductions: ${getTotalMonthlyDeductions()})      
-        ______________________________________________________________________
-                   PAYE: ${getMonthlyPAYE()}                
-                   PRSI: ${getMonthlyPRSI()}  
-                   Cycle To Work: ${cycleToWorkMonthlyDeduction}         
-        ______________________________________________________________________
-             NET PAY: ${getNetMonthlyPay()} 
-        ______________________________________________________________________"""
+       Monthly Payslip
+
+            Employee Information
+               - Name: ${getFullName()}
+               - ID: ${employeeID}
+
+            Payment Details
+               - Gross Pay: ${'$'}${getGrossMonthlyPay()}
+               - Salary: ${'$'}${getMonthlySalary()}
+               - Bonus: ${'$'}${roundTwoDecimals(annualBonus / 12)}
+
+            Deduction Details
+               - PAYE: ${'$'}${getMonthlyPAYE()}
+               - PRSI: ${'$'}${getMonthlyPRSI()}
+               - Cycle To Work: ${'$'}${cycleToWorkMonthlyDeduction}
+
+            Net Pay: ${'$'}${getNetMonthlyPay()}"""
 
     override fun toString(): String {
         return "Employee(firstName='$firstName', surname='$surname', gender=$gender, employeeID=$employeeID, grossSalary=$grossSalary, payePercentage=$payePercentage, prsiPercentage=$prsiPercentage, annualBonus=$annualBonus, cycleToWorkMonthlyDeduction=$cycleToWorkMonthlyDeduction)"
